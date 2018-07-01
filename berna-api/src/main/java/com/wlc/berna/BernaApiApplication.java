405ddb19a8318c600cbf8545a064196a2d2a38c9
zgwlc12138 <1069464349@qq.com>
2018-07-01 22:47:06 +0800

    init

---
+++ berna-api/src/main/java/com/wlc/berna/BernaApiApplication.java
@@ -0,0 +1,12 @@
+package com.wlc.berna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernaApiApplication.class, args);
+	}
+}
---
+++ berna-api/src/main/java/com/wlc/berna/service/impl/CityServiceImpl.java
@@ -0,0 +1,52 @@
+package com.wlc.berna.service.impl;
+
+import com.wlc.berna.service.CityService;
+import com.wlc.berna.vo.City;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CityServiceImpl implements CityService {
+
+    // 模拟数据库，存储 City 信息
+    private static Map<Long, City> CITY_DB = new HashMap<>();
+
+    @Override
+    public List<City> findAllCity() {
+        return new ArrayList<>(CITY_DB.values());
+    }
+
+    @Override
+    public City findCityById(Long id) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("findOneCity 2");
+        return CITY_DB.get(id);
+    }
+
+    @Override
+    public Long saveCity(City city) {
+        city.setId(CITY_DB.size() + 1L);
+        CITY_DB.put(city.getId(), city);
+        return city.getId();
+    }
+
+    @Override
+    public Long updateCity(City city) {
+        CITY_DB.put(city.getId(), city);
+        return city.getId();
+    }
+
+    @Override
+    public Long deleteCity(Long id) {
+        CITY_DB.remove(id);
+        return id;
+    }
+}
