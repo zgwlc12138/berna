a58d2683c4791fef978af29e118c4cfb3b9a64e3
CORP\zhanggong <zhanggong@ppdai.com>
2018-07-05 20:37:43 +0800

    初始化

--- berna-core/src/main/java/com/wlc/berna/core/test/ServerTest.java
+++ berna-core/src/main/java/com/wlc/berna/core/test/ServerTest.java
@@ -2,10 +2,7 @@ package com.wlc.berna.core.test;
 
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -13,8 +10,6 @@ import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.nio.charset.Charset;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @Author: zhanggong
@@ -33,9 +28,18 @@ public class ServerTest {
             protected void initChannel(SocketChannel channel) throws Exception {
                 channel.pipeline().addLast(new StringDecoder(Charset.forName("UTF-8")));
                 channel.pipeline().addLast(new StringEncoder(Charset.forName("UTF-8")));
-                channel.pipeline().addLast(new TestHandler());
+                channel.pipeline().addLast(new TestServerHandler());
             }
         }).option(ChannelOption.SO_BACKLOG, 1024*10).childOption(ChannelOption.SO_KEEPALIVE, true);
-        serverBootstrap.bind(9090);
+        ChannelFuture f= serverBootstrap.bind(9090);
+        try{
+            f.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
     }
 }
--- berna-core/src/main/java/com/wlc/berna/core/test/TestClientHandler.java
+++ berna-core/src/main/java/com/wlc/berna/core/test/TestClientHandler.java
@@ -2,30 +2,38 @@ package com.wlc.berna.core.test;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author: zhanggong
  * @Description:
- * @Date: Create in 16:42 2018/7/4
+ * @Date: Create in 19:58 2018/7/5
  * @Modified by:
  */
-public class TestHandler extends ChannelInboundHandlerAdapter {
+public class TestClientHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger logger=LoggerFactory.getLogger(TestClientHandler.class);
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channelRegistered");
         super.channelRegistered(ctx);
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        String s=(String)msg;
-        System.out.println("接收到消息："+s);
-        ctx.writeAndFlush("我是张弓");
+        logger.info("收到信息：{}",msg);
         super.channelRead(ctx, msg);
     }
 
     @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("张弓");
+        super.channelActive(ctx);
+    }
+
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error(cause.getMessage());
+        ctx.close();
         super.exceptionCaught(ctx, cause);
     }
 }
