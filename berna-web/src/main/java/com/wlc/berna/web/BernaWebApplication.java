0c341a821c1331cbb20749fb09cd66bbadab2b33
zgwlc12138 <1069464349@qq.com>
2018-07-21 13:52:32 +0800

    初始化

---
+++ berna-web/src/main/java/com/wlc/berna/web/BernaWebApplication.java
@@ -0,0 +1,12 @@
+package com.wlc.berna.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernaWebApplication.class, args);
+	}
+}
